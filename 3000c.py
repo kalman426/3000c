d2eb52b4383e9b38eaf2734eb4f32f36073e08e1
Olgipolgi <bodnar.oliver.17f@szlgbp.hu>
2022-01-05 09:45:28 +0100

    4.feladatig

---
+++ 3000c.py
@@ -0,0 +1,18 @@
+lista = []
+
+with open("input.txt", "r", encoding="utf8") as f:
+    for i in f:
+        lista.append(int(i))
+
+print("masodik feladat")
+m = 0
+for count, i in enumerate(lista):
+
+    if i%7 == 0:
+        m=count
+print(m)
+
+print("negyedik feladat")
+
+m = pow(sum(lista) % len(lista), 2)
+print(m)
